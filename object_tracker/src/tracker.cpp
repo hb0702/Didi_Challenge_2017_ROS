7e808bd42cbd1dd0b4e7673f7623ec6505ee4707
jaeil <parkjaeil0108@gmail.com>
2017-06-09 01:31:26 +0900

    more on nodes

--- object_tracker/src/tracker.cpp
+++ object_tracker/src/tracker.cpp
@@ -1,45 +1,74 @@
 #include <ros/ros.h>
-#include <sensor_msgs/PointCloud2.h>
-#include <pcl/point_cloud.h>
-#include <pcl/point_types.h>
-#include <pcl_conversions/pcl_conversions.h>
+#include <std_msgs/Float32MultiArray.h>
+#include <visualization_msgs/Marker.h>
+#include <visualization_msgs/MarkerArray.h>
 
-typedef pcl::PointXYZ _Point;
-typedef pcl::PointCloud<pcl::PointXYZ> _PointCloud;
-typedef _PointCloud::VectorType _PointVector;
-
-_PointCloud::Ptr cloud (new _PointCloud);
-
-void processLidarPoints(const sensor_msgs::PointCloud2::ConstPtr& msg)
+class Tracker
 {
-    pcl::fromROSMsg(*msg, *cloud);
-    _PointVector points = cloud->points;
-    size_t pointCount = points.size();
-    if (pointCount > 0)
+public:
+    Tracker()
     {
-        _Point front = points.front();
-        ROS_INFO("Got %zd points, first point: %.2f, %.2f, %.2f", pointCount, front.x, front.y, front.z);
-
-        // To do: process lidar points
-        // ...
+        ros::Subscriber sub = n.subscribe("/detected_boxes", 10, &Tracker::onBoxesReceived, this);
+        ros::Publisher pub = n.advertise<visualization_msgs::MarkerArray>("/bounding_boxes", 1);
     }
-}
+
+    ~Tracker()
+    {
+
+    }
+
+    void onBoxesReceived(const std_msgs::Float32MultiArray::ConstPtr& msg)
+    {
+        if (msg->data.size() < 1)
+        {
+            return;
+        }
+
+        visualization_msgs::MarkerArray arr;
+
+        for (auto it = msg->data.begin(); it != msg->data.end(); it+=8)
+        {
+            int label = int((*it) + 0.1);
+            float l = *(it+1);
+            float w = *(it+2);
+            float h = *(it+3);
+            float px = *(it+4);
+            float py = *(it+5);
+            float pz = *(it+6);
+            float yaw = *(it+7);
+
+            visualization_msgs::Marker marker;
+            marker.header.frame_id = "/my_frame"
+            marker.type = visualization_msgs::Marker::CUBE;
+            marker.action = visualization_msgs::Marker::ADD;
+            marker.pose.position.x = px;
+            marker.pose.position.y = py;
+            marker.pose.position.z = pz;
+            marker.pose.orientation.x = 0.0;
+            marker.pose.orientation.y = 0.0;
+            marker.pose.orientation.z = yaw;
+            marker.pose.orientation.w = 1.0;
+            marker.scale.x = l;
+            marker.scale.y = w;
+            marker.scale.z = h;
+            marker.color.a = 0.5;
+            marker.color.r = label == 0 ? 1.0 : 0.0;
+            marker.color.g = 0.0;
+            marker.color.b = label == 0 ? 0.0 : 1.0;
+            arr.markers.push_back(marker);
+        }
+
+        pub.publish(arr);
+        ROS_INFO("Tracker: published %d bounding boxes", arr.markers.size());
+    }
+};
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "lidar_points_listener");
+    ros::init(argc, argv, "tracker");
     ros::NodeHandle n;
 
-    ros::Subscriber sub = n.subscribe("/velodyne_points", 10, processLidarPoints);
-
-    // To do: subscribe image processing result
-    // ...
-
-    // To do: process lidar points
-    // ...
-
-    // To do: post process (publish result?)
-    // ...
+    Tracker tracker;
 
     ros::spin();
 
